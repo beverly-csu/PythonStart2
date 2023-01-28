cd7b9cd5941ac5758f3300576e858c35e8ce07ba
a4six <beverly_csu@mail.ru>
2023-01-28 14:16:59 +0500

    Update maze.py

--- PyGame/Maze/maze.py
+++ PyGame/Maze/maze.py
@@ -20,10 +20,37 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-hero = GameSprite('hero.png', 100, 200, 0)
-enemy = GameSprite('cyborg.png', 200, 200, 0)
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 430:
+            self.rect.y += self.speed
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 630:
+            self.rect.x += self.speed
+
+class Enemy(GameSprite):
+    direction = 'left'
+
+    def update(self):
+        if self.rect.x <= 520:
+            self.direction = 'right'
+        if self.rect.x >= 620:
+            self.direction = 'left'
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        if self.direction == 'right':
+            self.rect.x += self.speed
+
+hero = Player('hero.png', 100, 200, 5)
+enemy = Enemy('cyborg.png', 620, 300, 3)
 treasure = GameSprite('treasure.png', 400, 400, 0)
 
+rect1 = rect.Rect(x1, y1, width, height)
+rect1 = rect.Rect(x2, y2, width, height)
 # mixer.init()
 # mixer.music.load('jungles.ogg')
 # mixer.music.play()
@@ -36,7 +63,9 @@ while game:
 
     window.blit(background, (0, 0))
 
+    hero.update()
     hero.reset()
+    enemy.update()
     enemy.reset()
     treasure.reset()
 
